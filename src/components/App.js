78f37878c41be4319abdffd60f6f21deaa7847d3
Chris Davis <christopherjdavis417@gmail.com>
2019-03-09 16:50:13 -0500

    removed bootstrap, installed material-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { Route, HashRouter } from 'react-router-dom';
 
-import '../stylesheets/bootstrap.css';
-import '../stylesheets/bootstrap-grid.css';
-import '../stylesheets/bootstrap-reboot.css';
-
 import '../App.css';
 import '../index.css';
 
@@ -12,17 +8,19 @@ import Nav from './Nav';
 import Splash from './Splash';
 import Avengers from './Avengers';
 import Villians from './Villians';
+import ButtonAppBar from './ButtonAppBar';
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
-        <div>
+        <ButtonAppBar></ButtonAppBar>
+        {/* <div>
           <Nav />
             <Route exact path="/" component={Splash} />
             <Route path="/avengers" component={Avengers} />
             <Route path="/villians" component={Villians} />
-        </div>
+        </div> */}
       </HashRouter>
 
      
